176aff74c0f98480c9b1501e0eeb386b777f11a6
AlphaWaves <dev@awaves.it>
2023-12-28 00:09:27 +0100

    Spring initialization, maven pom, readme and docker env file config

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
-# bookz
+# Welcome to BOOKz!
+
+### Reference Documentation
+
+* docker run --name mysql_bookz --env-file /src/main/resources/bookz-mysql.env -p 3306:3306 -d mysql:latest
+
---
+++ src/test/java/com/vinai/bookz/BookzApplicationTests.java
@@ -0,0 +1,13 @@
+package com.vinai.bookz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BookzApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

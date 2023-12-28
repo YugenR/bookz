544997c89678e9a7f14442dc91b4b8b9f6e7e918
AlphaWaves <dev@awaves.it>
2023-12-28 21:51:07 +0100

    cors still giving problems

--- src/main/java/com/vinai/bookz/config/SecurityConfiguration.java
+++ src/main/java/com/vinai/bookz/config/SecurityConfiguration.java
@@ -1,37 +1,30 @@
 package com.vinai.bookz.config;
 
-import lombok.extern.slf4j.Slf4j;
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
+import org.springframework.security.config.annotation.method.configuration.GlobalMethodSecurityConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.config.http.SessionCreationPolicy;
-import org.springframework.security.web.authentication.www.BasicAuthenticationFilter;
+import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.web.DefaultSecurityFilterChain;
 
 
-@Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(prePostEnabled = true, securedEnabled = true)
-@Slf4j
-public class SecurityConfiguration extends WebSecurityConfigurerAdapter {
+@Configuration
+class SecurityConfiguration extends GlobalMethodSecurityConfiguration {
 
-    @Override
-    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+    @Bean
+    DefaultSecurityFilterChain configure(HttpSecurity http) throws Exception {
+        http.cors(Customizer.withDefaults());
+
+        http.csrf(AbstractHttpConfigurer::disable);
+
+
+        return http.build();
     }
 
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-
-        http = http
-                .cors()
-                .and()
-                .csrf().disable();
-
-        http.sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-
-    }
 
 }
